d94dd67529e11e8e9302d2e1b42707ae05f67a63
PaulFenton <paulfenton@dal.ca>
2019-01-21 08:11:46 -0600

    updated port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var fs = require('fs');
 const app = express()
-const port = 3000
+const port = 80
 
 //configure CORS for local development
 app.use(function(req, res, next) {
